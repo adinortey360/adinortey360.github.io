840cf4660c5b576a35bdbf75f63d49fe31d9df34
Adinortey Lawerteh <49171248+adinortey360@users.noreply.github.com>
2022-05-04 01:24:34 +0000

    iterate experience

--- js/script.js
+++ js/script.js
@@ -22,4 +22,9 @@ $.ajax({
     content.recommendations.forEach(element => {
       $("#recommendations").append('<div class="slick-track" > <div data-index="4" tabindex="-1" class="slick-slide slick-cloned" aria-hidden="true" style="width: 568px;"> <div> <div tabindex="-1" style="width: 100%; display: inline-block;" class="aos-init"> <div class="testimonial-01 media"> <div class="avatar"><img src="'+element.image+'" alt="review comments"></div> <div class="media-body"> <p>'+element.message+'</p> <h6>'+element.author+'</h6><span>'+element.author_title+'</span> </div> </div> </div> </div> </div> </div>');
     });
+
+
+    content.experience.forEach(element => {
+      $("#experience").append('<div class="resume-row aos-init"> <div class="row"> <div class="col-md-4 col-xl-3"> <div class="rb-left"> <h6>'+element.designation+'</h6><label>'+element.company_shortname+' | '+element.role+'</label> <p>'+element.from+' - '+element.to+'</p> <div class="rb-time">'+element.job_type+'</div> </div> </div> <div class="col-md-8 col-xl-9"> <div class="rb-right"> <h6>'+element.company+'</h6> <p>'+element.job_description+'</p> </div> </div> </div> </div>');
+    });
   });
