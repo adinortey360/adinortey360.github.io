beed9028e2dd2cba7f17ce5e1ae3e5d2578c242a
Adinortey Lawerteh <adinortey360@gmail.com>
2022-05-04 00:46:04 +0000

    iterate append awards and certs

--- js/script.js
+++ js/script.js
@@ -5,4 +5,17 @@ $.ajax({
     $("#greeting_name").html(content.greeting_name);
     $("#job_title").html(content.job_title);
     $("#short_bio").html(content.short_bio);
+    $("#full_bio").html(content.bio.full_bio);
+    $("#bio_name").html(content.bio.extras.name);
+    $("#bio_location").html(content.bio.extras.location);
+    $("#bio_interests").html(content.bio.extras.interests);
+    $("#bio_hobbies").html(content.bio.extras.hobbies);
+    $("#bio_phone").html(content.bio.extras.phone);
+    $("#bio_email").html(content.bio.extras.email);
+    $("#bio_languages").html(content.bio.extras.languages);
+    $("#bio_fun_fact").html(content.bio.extras.fun_fact);
+
+    content.awards_certs.forEach(element => {
+        $("#award_certs").append('<div class="col-lg-4 m-15px-tb aos-init" > <div class="feature-box-02 d-flex align-items-center"> <div class="icon"><img src="'+element.image+'" alt="award"></div> <div class="media-body"> <h6>'+element.title+'</h6> <p>'+element.subtitle+'</p> </div> </div> </div>');
+    });
   });
