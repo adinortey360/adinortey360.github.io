cd7437ab824a4a2c1826ea3415f91e3285cceccd
Adinortey Lawerteh <adinortey360@gmail.com>
2022-05-12 01:30:47 +0000

    Fix duplication on language change

--- js/script.js
+++ js/script.js
@@ -7,6 +7,14 @@ function initPage() {
   if(storedlang != null) {
     lang = storedlang;
   }
+
+  if(lang == "en") {
+    $("#lang_en").addClass("active");
+    $("#lang_fr").removeClass("active");
+  } else {
+    $("#lang_fr").addClass("active");
+    $("#lang_en").removeClass("active");
+  }
   
   $.ajax({
     url: "/lang/content_"+storedlang+".json"
@@ -33,27 +41,33 @@ function initPage() {
     $("#contact_emailtop").html(content.contacts.email);
     $("#contact_address").html(content.contacts.address);
   
+    $("#award_certs").html("");
     content.awards_certs.forEach(element => {
       $("#award_certs").append('<div class="col-lg-4 m-15px-tb aos-init" > <div class="feature-box-02 d-flex align-items-center"> <div class="icon"><img src="' + element.image + '" alt="award"></div> <div class="media-body"> <h6>' + element.title + '</h6> <p>' + element.subtitle + '</p> </div> </div> </div>');
     });
   
+    $("#recommendations").html("");
     content.recommendations.forEach(element => {
       $("#recommendations").append('<div class="single-recommendation" > <div data-index="4" tabindex="-1" class="slick-slide slick-cloned" aria-hidden="true" style="width: 100%;"> <div> <div tabindex="-1" style="width: 100%; display: inline-block;" class="aos-init"> <div class="testimonial-01 media"> <div class="avatar"><img src="' + element.author_image + '" alt="review comments"></div> <div class="media-body"> <p style=" font-size: 20px; font-weight: 400; ">' + element.message + '</p> <h6>' + element.author + '</h6><span>' + element.author_title + '</span> </div> </div> </div> </div> </div> </div>');
     });
   
   
+    $("#experience").html("");
     content.experience.forEach(element => {
       $("#experience").append('<div class="resume-row aos-init"> <div class="row"> <div class="col-md-4 col-xl-3"> <div class="rb-left"> <h6>' + element.designation + '</h6><label>' + element.company_shortname + ' | ' + element.role + '</label> <p>' + element.from + ' - ' + element.to + '</p> <div class="rb-time">' + element.job_type + '</div> </div> </div> <div class="col-md-8 col-xl-9"> <div class="rb-right"> <h6>' + element.company + '</h6> <p>' + element.job_description + '</p> </div> </div> </div> </div>');
     });
   
+    $("#education").html("");
     content.education.forEach(element => {
       $("#education").append('<li><span>' + element.from + '-' + element.to + '</span> <h6>' + element.course_title + '</h6> <p>' + element.institution + '</p> </li>');
     });
   
+    $("#skill").html("");
     content.skills.forEach(element => {
       $("#skill").append('<div class="skill-lt"> <h6>' + element.skill + '</h6><span class="count-inner"><span>' + element.rating.toString() + '</span>%</span> <div class="skill-bar"> <div class="skill-bar-in" style="width: ' + element.rating.toString() + '%;"></div> </div> </div>');
     });
   
+    $("#projects").html("");
     content.projects.forEach(element => {
       $("#projects").append(
         '<div class="col-md-6">' +
@@ -89,7 +103,7 @@ function initPage() {
       );
     });
   
-  
+    $("#lastest_posts").html("");
     content.lastest_posts.forEach(element => {
       $("#lastest_posts").append('<div class="col-md-6 m-15px-tb aos-init" > <div class="blog-grid"> <div class="blog-img"><img src="' + element.image + '" alt="blog post"></div> <div class="blog-info"> <div class="meta">' + element.date + ' - ' + element.platform + '</div> <h6><a href="' + element.link + '" style="cursor: none;">' + element.title + '</a></h6> </div> </div> </div>');
     });
